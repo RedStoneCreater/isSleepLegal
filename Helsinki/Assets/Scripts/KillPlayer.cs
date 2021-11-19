edb0ff711ec6aa73e0066416d10974468e1fc5f0
SannaRichmond <Richmondshanon@gmail.com>
2021-11-19 21:56:41 +1300

    Assets imported and laid out

---
+++ Helsinki/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    public GameObject killCam;
+    public GameObject player;
+    public GameObject mCam;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if(collision.gameObject.tag == "Player" )
+        {
+            mCam.GetComponent<AudioListener>().enabled = false;
+            Destroy(collision.gameObject);
+            Instantiate(killCam, this.transform);
+        }
+    }
+}
---
+++ Helsinki/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int coincount;
+    public float PlayerHealth;
+
+    public bool PickUpItem(GameObject obj)
+    {
+        switch (obj.tag)
+        {
+            case "AccessCoin":
+                coincount++;
+                return true;
+                
+
+            default:
+                Debug.LogWarning($"WARNING: No Handler implemented for tag {obj.tag}.");
+                return false;
+
+            case "HealthOrb":
+                PlayerHealth += 10;
+                return true;
+        }
+        
+    }
+}
